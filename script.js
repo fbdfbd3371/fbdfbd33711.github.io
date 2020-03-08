e230f9b35da14ad82b394ac254e7a6260714adc3
fbdfbd3371 <54417081+fbdfbd3371@users.noreply.github.com>
2020-03-08 21:30:30 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,5 @@
+
+jQuery(document).ready(function($) {
+    $( "body" ).append( "<a href = 'https://vk.com/alinkasocool'>Ну попробуй, перейди</a>" );
+    $("body").append("<a href = 'https://vk.com/id516728410'>Давай,давай</a>");
+});
